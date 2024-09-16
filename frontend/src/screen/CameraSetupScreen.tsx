caa519081c961789c11750b10e3efdb85e37703c
YokotaYusuke <tarut8166@gmail.com>
2024-09-16 21:37:59 +0900

    Amplifyの設定変更2

--- frontend/src/screen/CameraSetupScreen.tsx
+++ frontend/src/screen/CameraSetupScreen.tsx
@@ -1,6 +1,8 @@
 import {useNavigate} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import {signInWithRedirect} from '@aws-amplify/auth'
+import { Hub } from 'aws-amplify/utils'
+import { getCurrentUser } from 'aws-amplify/auth'
 
 export default function CameraSetupScreen() {
   const navigate = useNavigate()
@@ -8,15 +10,31 @@ export default function CameraSetupScreen() {
 
   const apiUrl = import.meta.env.VITE_API_URL
 
+  Hub.listen('auth', async ({ payload }) => {
+    switch(payload.event) {
+      case 'signInWithRedirect':
+        const user = await getCurrentUser()
+        console.log(user.username)
+        break
+      case 'signInWithRedirect_failure':
+        break
+      case 'customOAuthState':
+        const state = payload.data
+        console.log(state)
+        break
+    }
+  })
+
   useEffect(() => {
     fetch(apiUrl)
       .then(res => res.json())
       .then(data => setLambda(data))
   }, [])
 
-  const loginWithSocialAccount = async () => {
+  const loginWithSocialAccount = async (customState: string) => {
     await signInWithRedirect({
       provider: 'Google',
+      customState
     })
   }
 
@@ -24,7 +42,7 @@ export default function CameraSetupScreen() {
     <>
       <h2>バーコード読み取り</h2>
       <div>{lambda}</div>
-      <button onClick={loginWithSocialAccount}>Googleでログイン</button>
+      <button onClick={() => loginWithSocialAccount}>Googleでログイン</button>
       <button onClick={() => navigate('/scan')}>カメラを起動する</button>
     </>
   )
