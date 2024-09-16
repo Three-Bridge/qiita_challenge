dbc7abe76186c59f37ca29ffd2e59390ca78508a
YokotaYusuke <tarut8166@gmail.com>
2024-09-16 22:23:22 +0900

    Amplifyの設定変更4

--- frontend/src/screen/CameraSetupScreen.tsx
+++ frontend/src/screen/CameraSetupScreen.tsx
@@ -1,14 +1,30 @@
 import {useNavigate} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import {signInWithRedirect} from '@aws-amplify/auth'
-import { Hub } from 'aws-amplify/utils'
-import {AuthUser, getCurrentUser } from 'aws-amplify/auth'
+import {Amplify} from 'aws-amplify'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'ap-northeast-1_TUBhSTpkw',
+      userPoolClientId: 'j8kkhbqirvs8l3i181sind02j',
+      loginWith: {
+        oauth: {
+          domain: 'https://barcode-battler.auth.ap-northeast-1.amazoncognito.com',
+          scopes: ['openid', 'email', 'profile'],
+          redirectSignIn: ['https://qiita-challenge-1.onrender.com/callback'],
+          redirectSignOut: ['https://qiita-challenge-1.onrender.com'],
+          responseType: 'code',
+          providers: ['Google']
+        }
+      }
+    }
+  }
+})
 
 export default function CameraSetupScreen() {
   const navigate = useNavigate()
   const [lambda, setLambda] = useState<string | null>(null)
-  const [user, setUser] = useState<AuthUser | null>(null)
-  const [customState, setCustomState] = useState<string>('')
 
   const apiUrl = import.meta.env.VITE_API_URL
 
@@ -17,39 +33,11 @@ export default function CameraSetupScreen() {
       .then(res => res.json())
       .then(data => setLambda(data))
 
-    const unsubscribe = Hub.listen('auth', async ({ payload }) => {
-      switch(payload.event) {
-        case 'signInWithRedirect':
-          getUser()
-          break
-        case 'signInWithRedirect_failure':
-          console.log("An error has occurred during the Oauth flow.")
-          break
-        case 'customOAuthState':
-          setCustomState(payload.data)
-          break
-      }
-    })
-
-    getUser()
-
-    return unsubscribe
   }, [])
 
-  const getUser = async (): Promise<void> => {
-    try {
-      const currentUser = await getCurrentUser()
-      setUser(currentUser)
-    } catch (error) {
-      console.log(error)
-      console.error("Not Signed in")
-    }
-  }
-
-  const loginWithSocialAccount = async (customState: string) => {
+  const loginWithSocialAccount = async () => {
     await signInWithRedirect({
-      provider: 'Google',
-      customState
+      provider: 'Google'
     })
   }
 
@@ -57,8 +45,7 @@ export default function CameraSetupScreen() {
     <>
       <h2>バーコード読み取り</h2>
       <div>{lambda}</div>
-      <div>{user?.username || 'ログイン未'}</div>
-      <button onClick={() => loginWithSocialAccount(customState)}>Googleでログイン</button>
+      <button onClick={() => loginWithSocialAccount()}>Googleでログイン</button>
       <button onClick={() => navigate('/scan')}>カメラを起動する</button>
     </>
   )
