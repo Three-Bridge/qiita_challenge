dfe43f809c67986cc3480879c1c2d400845e2e3f
YokotaYusuke <tarut8166@gmail.com>
2024-09-16 21:50:51 +0900

    Amplifyの設定変更3

--- frontend/src/screen/CameraSetupScreen.tsx
+++ frontend/src/screen/CameraSetupScreen.tsx
@@ -2,34 +2,49 @@ import {useNavigate} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import {signInWithRedirect} from '@aws-amplify/auth'
 import { Hub } from 'aws-amplify/utils'
-import { getCurrentUser } from 'aws-amplify/auth'
+import {AuthUser, getCurrentUser } from 'aws-amplify/auth'
 
 export default function CameraSetupScreen() {
   const navigate = useNavigate()
   const [lambda, setLambda] = useState<string | null>(null)
+  const [user, setUser] = useState<AuthUser | null>(null)
+  const [customState, setCustomState] = useState<string>('')
 
   const apiUrl = import.meta.env.VITE_API_URL
-
-  Hub.listen('auth', async ({ payload }) => {
-    switch(payload.event) {
-      case 'signInWithRedirect':
-        const user = await getCurrentUser()
-        console.log(user.username)
-        break
-      case 'signInWithRedirect_failure':
-        break
-      case 'customOAuthState':
-        const state = payload.data
-        console.log(state)
-        break
-    }
-  })
 
   useEffect(() => {
     fetch(apiUrl)
       .then(res => res.json())
       .then(data => setLambda(data))
+
+    const unsubscribe = Hub.listen('auth', async ({ payload }) => {
+      switch(payload.event) {
+        case 'signInWithRedirect':
+          getUser()
+          break
+        case 'signInWithRedirect_failure':
+          console.log("An error has occurred during the Oauth flow.")
+          break
+        case 'customOAuthState':
+          setCustomState(payload.data)
+          break
+      }
+    })
+
+    getUser()
+
+    return unsubscribe
   }, [])
+
+  const getUser = async (): Promise<void> => {
+    try {
+      const currentUser = await getCurrentUser()
+      setUser(currentUser)
+    } catch (error) {
+      console.log(error)
+      console.error("Not Signed in")
+    }
+  }
 
   const loginWithSocialAccount = async (customState: string) => {
     await signInWithRedirect({
@@ -42,7 +57,8 @@ export default function CameraSetupScreen() {
     <>
       <h2>バーコード読み取り</h2>
       <div>{lambda}</div>
-      <button onClick={() => loginWithSocialAccount}>Googleでログイン</button>
+      <div>{user?.username || 'ログイン未'}</div>
+      <button onClick={() => loginWithSocialAccount(customState)}>Googleでログイン</button>
       <button onClick={() => navigate('/scan')}>カメラを起動する</button>
     </>
   )
