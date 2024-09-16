154e1279cbbbe033f81d4b93af60a0be98188766
YokotaYusuke <tarut8166@gmail.com>
2024-09-17 00:10:43 +0900

    認証済みのユーザーを表示する

--- frontend/src/screen/CameraSetupScreen.tsx
+++ frontend/src/screen/CameraSetupScreen.tsx
@@ -2,7 +2,7 @@ import {useNavigate} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import {signInWithRedirect} from '@aws-amplify/auth'
 import {Amplify} from 'aws-amplify'
-import { signOut } from 'aws-amplify/auth/cognito'
+import {AuthUser, getCurrentUser, signOut } from 'aws-amplify/auth/cognito'
 
 Amplify.configure({
   Auth: {
@@ -27,11 +27,14 @@ export default function CameraSetupScreen() {
   const navigate = useNavigate()
   const [lambda, setLambda] = useState<string | null>(null)
   const apiUrl = import.meta.env.VITE_API_URL
+  const [authUser, setAuthUser] = useState<AuthUser | null>(null)
 
   useEffect(() => {
     fetch(apiUrl)
       .then(res => res.json())
       .then(data => setLambda(data))
+
+    currentAuthenticatedUser()
   }, [])
 
   const loginWithSocialAccount = async () => {
@@ -49,10 +52,20 @@ export default function CameraSetupScreen() {
     })
   }
 
+  async function currentAuthenticatedUser() {
+    try {
+      const user = await getCurrentUser()
+      setAuthUser(user)
+    } catch (error) {
+      console.log('未ログイン: ',error)
+    }
+  }
+
   return (
     <>
       <h2>バーコード読み取り</h2>
       <div>{lambda}</div>
+      <div>{authUser?.username || '未ログイン'}</div>
       <button onClick={() => loginWithSocialAccount()}>Googleでログイン</button>
       <button onClick={() => signOutWithSocialAccount()}>サインアウト</button>
       <button onClick={() => navigate('/scan')}>カメラを起動する</button>
