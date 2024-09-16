65e5e3e520f2ebcf57d11d8ef1e685b7d7aa03ae
YokotaYusuke <tarut8166@gmail.com>
2024-09-16 21:06:41 +0900

    Amplifyの設定変更

--- frontend/src/aws-exports.ts
+++ frontend/src/aws-exports.ts
@@ -4,10 +4,10 @@ Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: 'ap-northeast-1_TUBhSTpkw',
-      userPoolClientId: 'ap-northeast-1_TUBhSTpkw',
+      userPoolClientId: 'j8kkhbqirvs8l3i181sind02j',
       loginWith: {
         oauth: {
-          domain: 'my-app-auth.auth.ap-northeast-1.amazoncognito.com',
+          domain: 'https://barcode-battler.auth.ap-northeast-1.amazoncognito.com',
           scopes: ['openid', 'email', 'profile'],
           redirectSignIn: ['https://qiita-challenge-1.onrender.com/callback'],
           redirectSignOut: ['https://qiita-challenge-1.onrender.com'],
