c570192e2bd9416121bf596a32ad66a2b83f4c05
shogo <shogo_inoue@mail.toyota.co.jp>
2024-09-19 22:24:28 +0900

    スライドバーのpackageJsonを修正

--- frontend/src/sliderBar/sliderBar.tsx
+++ frontend/src/sliderBar/sliderBar.tsx
@@ -18,7 +18,7 @@ const Range = styled.input.attrs({
     &::-webkit-slider-thumb {
         -webkit-appearance: none;
         appearance: none;
-        width: 20px;
+        width: 50px;
         height: 20px;
         background-color: #4caf50;
         cursor: pointer;
