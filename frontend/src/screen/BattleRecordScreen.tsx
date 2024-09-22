b34a4e1011c83761827d13ee30b75513fed1f169
Heegwon Im <gwony@Gwony-MacBook-Pro-M3.local>
2024-09-22 11:55:12 +0900

    result page design

--- frontend/src/screen/BattleRecordScreen.tsx
+++ frontend/src/screen/BattleRecordScreen.tsx
@@ -1,33 +1,50 @@
 import {Authenticator} from '@aws-amplify/ui-react'
-import {useLocation} from 'react-router-dom'
-// importy style from
+import {useLocation, useNavigate} from 'react-router-dom'
+import style from './BattleRecordScreen.module.scss'
 
 export default function BattleRecordScreen() {
+  const navigator = useNavigate();
   const location = useLocation()
   const responseObject = location.state
 
   return (
     <Authenticator
       socialProviders={['google', 'amazon', 'apple', 'facebook']}>
-      <h1>戦績</h1>
-      <table border={1}>
-        <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">勝敗</th>
-          <th scope="col">戦闘日</th>
-        </tr>
-        </thead>
-        <tbody>
-        {responseObject.map((ele,index) => (
-          <tr key = {index}>
-            <th>{index+1}</th>
-            <th scope="row">{ele['battle-result']?"Win":"Lose"}</th>
-            <td>{ele['battle-date']}</td>
+      <div className={style.body}>
+      <h1>History</h1>
+      <div className={style.spacer}></div>
+      <div className={style.tableContainer}>
+        <table border={1}>
+          <thead>
+          <tr>
+            <th className={style.idRow} scope="col">ID</th>
+            <th className={style.resultRow} scope="col">Result</th>
+            <th className={style.dateRow} scope="col">Date</th>
           </tr>
-        ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+          {responseObject.map((ele,index) => (
+            <tr key = {index}>
+              <td >{index+1}</td>
+              <td className={style.result} scope="row">{ele['battle-result']?"Win":"Lose"}</td>
+              <td>
+                <p>{ele['battle-date'].slice(0,10)}</p>
+                <p>{ele['battle-date'].slice(11,19)}</p>
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+        <div className={style.spacer}></div>
+      </div>
+        <div className={style.buttonContainer}>
+
+      <button onClick={()=>{
+        navigator("/auth")
+      }}>最初から</button>
+        </div>
+
+      </div>
     </Authenticator>
   )
 }
